93a5a1a55a92d115a075ca4b4d9deb49c4e63b15
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2019-02-10 22:13:36 +0700

    Add files via upload

---
+++ emp-service.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/empservice.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	<link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500|Rajdhani:400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700|ZCOOL+KuaiLe" rel="stylesheet"> 
+</head>
+<body>
+	<div class="profile-page">
+		<div class="background">
+			<div class="inside-background">
+				<header>
+					<a href="index.php" class="logo">EMP</a>
+					<div class="menu-toggle"></div>
+					<nav>
+						<ul> 
+							<li>
+								<a href="index.php" class="home">Home</a>
+							</li>
+							<li>
+								<a href="emp-service.php" class="emp-services">EMP Services</a>
+							</li>
+							<li>
+								<a href="profile.php" class="about">Profile</a>
+							</li>
+						</ul>
+					</nav>
+					<div class="clearfix"></div>
+				</header>
+				<h1 class="h1-emp-s">EMP Services</h1>
+			</div>
+			<!-- EMP Services -->
+			<div class="emp-services">
+				<div class="inner-es">
+					<div class="emp-service">
+					<div class="content">
+						<div class="graphic-design-content">
+							<div class="border-gd">
+								<img src="images/graphic_design.jpg" class="img-gd">
+								<div class="text-gd">
+								<h1 class="h1-gd">Graphic Design</h1>
+								<p class="p-gd">
+									<ul>
+										<li class="dp">Poster Design </li>
+										<li class="dp">Banner Design </li>
+										<li class="dp">Photo Design</li>
+										<li class="dp">Logo Design </li>
+									</ul>
+								<p class="under-list">All work is done with the level of difficulty and request of the client. The more difficult the level of making a design, the longer the process will be. Just contact me for more info. My contact is at the bottom of the page.
+								</p>
+								</p>
+								</div>
+							</div>
+						</div>
+						<div class="vectore-design-content">
+							<div class="vd-content">
+								<div class="border-vd">
+									<img src="images/vector_design.jpg" class="img-vd">
+									<div class="text-vd">
+									<h1 class="h1-vd">Graphic Design</h1>
+									<p class="p-vd">
+										<ul>
+											<li class="dp">Poster Design </li>
+											<li class="dp">Banner Design </li>
+											<li class="dp">Photo Design</li>
+											<li class="dp">Logo Design </li>
+										</ul>
+									<p class="under-list">All work is done with the level of difficulty and request of the client. The more difficult the level of making a design, the longer the process will be. Just contact me for more info. My contact is at the bottom of the page.
+									</p>
+									</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="video-design-content">
+							<div class="vd-content">
+								<div class="border-vd">
+									<img src="images/video_editor2.jpg" class="img-vid">
+									<div class="text-vd">
+									<h1 class="h1-vd">Videographer</h1>
+									<p class="p-vd">
+										<ul>
+											<li class="dp">Taking Video Footage</li>
+											<li class="dp">Narrative Making</li>
+											<li class="dp">Video Editing</li>
+											<li class="dp">Bumper Making</li>
+											<li class="dp">Motion Graphic Making</li>
+										</ul>
+									<p class="under-list">All work is done with the level of difficulty and request of the client. The more difficult the level of making a design, the longer the process will be. Just contact me for more info. My contact is at the bottom of the page.
+									</p>
+									</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="web-design-content">
+							<div class="vd-content">
+								<div class="border-vd">
+									<img src="images/web_developer.jpg" class="img-wd">
+									<div class="text-vd">
+									<h1 class="h1-vd">Web Developer</h1>
+									<p class="p-vd">
+										<ul>
+											<li class="dp">Wireframing Website</li>
+											<li class="dp">Mockup Design</li>
+											<li class="dp">Front-End</li>
+											<li class="dp">Back-End</li>
+										</ul>
+									<p class="under-list">All work is done with the level of difficulty and request of the client. The more difficult the level of making a design, the longer the process will be. Just contact me for more info. My contact is at the bottom of the page.
+									</p>
+									</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Contact -->
+			<div class="contact">
+				<div class="inner-contact">
+					<h1 class="h1-contact">My Contact</h1>
+					<div class="inside" align="center">
+						<div class="content">
+							<div class="phone" align="center">
+								<i class="fas fa-phone"></i>
+							</div>
+							<p class="p-phone"> +623833808653 </p>
+						</div>
+						<div class="content">
+							<div class="phone" align="center">
+								<i class="fas fa-envelope"></i>
+							</div>
+							<p class="p-phone">enrichoalkalas02@gmail.com</p>
+						</div>
+						<div class="content">
+							<div class="phone" align="center">
+								<i class="fab fa-instagram""></i>
+							</div>
+							<p class="p-phone">enrichoalkalas02</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	<link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500|Rajdhani:400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700|ZCOOL+KuaiLe" rel="stylesheet"> 
+</head>
+<body>
+	<div class="profile-page">
+		<div class="background">
+			<div class="inside-background">
+				<header>
+					<a href="index.php" class="logo">EMP</a>
+					<div class="menu-toggle"></div>
+					<nav>
+						<ul> 
+							<li>
+								<a href="index.php" class="home">Home</a>
+							</li>
+							<li>
+								<a href="emp-service.php" class="emp-services">EMP Services</a>
+							</li>
+							<li>
+								<a href="profile.php" class="about">Profile</a>
+							</li>
+						</ul>
+					</nav>
+					<div class="clearfix"></div>
+				</header>
+				<h1 class="h1-view-page1">Welcome To My Website</h1>
+			</div>
+			<!-- EMP Services -->
+			<div class="under-insdie-background">
+				<div class="emp-service">
+					<h1 class="h1-emp-services">EMP Services</h1>
+					<p class="p-emp-services">The following services that our provide to costumers</p>
+					<div class="link-emp-services">
+						<div class="images" align="center">
+							<img src="images/graphic_design.jpg" class="img-emp-services">
+							<img src="images/vector_design.jpg" class="img-emp-services">
+							<img src="images/video_editor2.jpg" class="img-emp-services">
+							<img src="images/web_developer.jpg" class="img-emp-services">
+						</div>
+					</div>
+					<div class="button-emp" align="center">
+						<a href="emp-service.php" class="emp-services-button">EMP Services</a>
+					</div>
+				</div>
+			</div>
+			<!-- Portofolio -->
+			<div class="portofolio-container">
+				<div class="inside-pc">
+					<h1 class="h1-portofolio">Portofolio</h1>
+					<div class="div-p-port">
+						<p class="p-portofolio">
+							All the works that I have made can be seen on websites that have become a reference in putting in works that have been made. Here is the media platform that I use to put all my works
+						</p>
+					</div>
+					<div class="medsos">
+						<div class="middle-sm">
+						<a class="btn" href="https://www.instagram.com/alkariko_ajax/" target="_blank">
+							<i class="fab fa-instagram"></i>
+						</a>
+						<a class="btn" href="https://www.behance.net/enrichoalk5fcf" target="_blank">
+							<i class="fab fa-behance"></i>
+						</a>
+						<a class="btn" href="https://www.instagram.com/enrichoalkalas/" target="_blank">
+							<i class="fab fa-github"></i>
+						</a>
+						<a class="btn" href="https://id.pinterest.com/enrichoalkalas/my-portofolio/" target="_blank">
+							<i class="fab fa-pinterest"></i>
+						</a>
+						<a class="btn" href="https://www.youtube.com/channel/UCT_qzoVVQHoKuPnuu4N-4OA" target="_blank">
+							<i class="fab fa-youtube"></i>
+						</a>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+</body>
+</html>
